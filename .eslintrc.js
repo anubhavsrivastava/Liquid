fc35c0525e8f9720d850ee1e431a6cdd83de8ed7
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-24 00:08:54 +0530

    chore<lint> : added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true
+		}
+	},
+	extends: 'eslint:recommended'
+};
