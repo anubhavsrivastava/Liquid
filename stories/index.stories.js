99d92f3a9c90000d0519774df3ac5090deb52d0e
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-25 00:08:53 +0530

    feat<storybook> : import button

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,16 +4,16 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
+import { Welcome } from '@storybook/react/demo';
+import Button from '../src/Button/Button';
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+	.add('with text', () => <Button onClick={action('clicked')} value="text" />)
+	.add('with some emoji', () => (
+		<Button onClick={action('clicked')}>
+			<span role="img" aria-label="so cool">
+				😀 😎 👍 💯
+			</span>
+		</Button>
+	));
