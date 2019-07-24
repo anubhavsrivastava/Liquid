501fe0973f4ce1ca94821e00127bf2fb32ffa109
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-25 00:04:59 +0530

    chore<storybook> : theming done

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,32 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+import theme from './theme';
+addParameters({
+	options: {
+		isFullscreen: false,
+
+		showNav: true,
+
+		showPanel: true,
+
+		panelPosition: 'bottom',
+
+		hierarchySeparator: /\/|\./,
+
+		hierarchyRootSeparator: /\|/,
+
+		sidebarAnimations: true,
+
+		enableShortcuts: true,
+
+		isToolshown: false,
+		theme: theme
+	}
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+	req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
---
+++ .storybook/theme.js
@@ -0,0 +1,38 @@
+import { create } from '@storybook/theming';
+import logo from '../media/banner_white.svg';
+
+export default create({
+	base: 'dark',
+
+	// colorPrimary: 'hotpink',
+	// colorSecondary: 'deepskyblue',
+
+	// UI
+	// appBg: 'white',
+	// appContentBg: 'silver',
+	// appBorderColor: 'grey',
+	// appBorderRadius: 4,
+
+	// Typography
+	// fontBase: '"Open Sans", sans-serif',
+	// fontCode: 'monospace',
+
+	// Text colors
+	// textColor: 'black',
+	// textInverseColor: 'rgba(255,255,255,0.9)',
+
+	// Toolbar default and active colors
+	// barTextColor: 'silver',
+	// barSelectedColor: 'black',
+	// barBg: 'hotpink',
+
+	// Form colors
+	// inputBg: 'white',
+	// inputBorder: 'silver',
+	// inputTextColor: 'black',
+	// inputBorderRadius: 4,
+
+	brandTitle: 'Liquid UI',
+	brandUrl: 'https://liquidui.netlify.com',
+	brandImage: logo
+});
